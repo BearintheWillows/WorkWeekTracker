a3723cc7e8dfa367aceda367e08a8cfaef5a56ba
BearintheWillows <sjwillows91@outlook.com>
2022-09-24 09:50:11 +0100

    Added slide animation to router-outlet

--- WWTAngular/src/app/routing-modules/runs-routing.module.ts
+++ WWTAngular/src/app/routing-modules/runs-routing.module.ts
@@ -1,13 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import {RunDetailComponent} from "../Runs/run-detail/run-detail.component";
 import {RunListComponent} from "../Runs/run-list/run-list.component";
 
 const runsRoutes: Routes = [
 
-  { path: 'runs', component: RunListComponent},
-  { path: 'run/:id/details', component: RunDetailComponent},
+  {
+    path: 'runs', component: RunListComponent,
+    data: {animation: 'runs'}
+  },
+  {
+    path: 'run/:id/details', component: RunDetailComponent,
+    data: {animation: 'run'}
+  },
 
 ];
 
@@ -16,4 +22,5 @@ const runsRoutes: Routes = [
   imports: [RouterModule.forChild(runsRoutes)],
   exports: [RouterModule]
 })
-export class RunsRoutingModule { }
+export class RunsRoutingModule {
+}
